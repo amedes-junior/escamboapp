29d7bc8b8871f95b8367229111cf70ae7edd8d0d
Amedes Eleuterio da Costa Junior <9817@TTJDEINFU181784.tj.ce.gov.br>
2019-02-01 18:14:22 -0300

    After Classes 205, 206, 207, 208, 209, 210 - Feb 01, 2019
    After Classes 205, 206, 207, 208, 209, 210 - Feb 01, 2019

--- app/models/member.rb
+++ app/models/member.rb
@@ -9,10 +9,23 @@ class Member < ActiveRecord::Base
   # Associations
   has_many :ads
 
+  #Validations
+  validate :nested_attributes
+
   has_one :profile_member
   accepts_nested_attributes_for :profile_member
 
   #Ratuyrate gem
   ratyrate_rater
+
+  def nested_attributes
+    if nested_attributes_blank?
+      errors.add(:base, "É necessário preencher Primeiro e Segundo nomes")
+    end
+  end
+
+  def nested_attributes_blank?
+    profile_member.first_name.blank? || profile_member.second_name.blank?
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -40,11 +40,17 @@ puts "ADMINISTRADOR cadastrado com sucesso"
 
 puts "Cadastrando MEMBRO Padrão"
 
-Member.create!(
+member = Member.new(
   email: "membro@membro.com",
   password: "123456",
   password_confirmation: "123456"
   )
+
+  member.build_profile_member
+  member.profile_member.first_name = Faker::Name.first_name
+  member.profile_member.second_name = Faker::Name.last_name
+
+  member.save!
 
 puts "MEMBRO padrão cadastrado com sucesso!"
 
